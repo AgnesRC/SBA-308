ad72a0f9b22af22ace6f6b056cc60d3cd8ce5a90
Katrascia <ar4311@tc.columbia.edu>
2025-09-05 14:03:52 -0400

    set new variables

--- script.js
+++ script.js
@@ -99,4 +99,50 @@ const CourseInfo = {
   const result = getLearnerData(CourseInfo, AssignmentGroup, LearnerSubmissions);
   
   console.log(result);
-  
+   
+
+
+// My Work Begins 
+
+// Identify all data needed for the string 
+console.log('Learner IDs');
+console.log(LearnerSubmissions[0].learner_id);
+console.log(LearnerSubmissions[3].learner_id);
+
+console.log('Assignment Possible Scores');
+console.log(AssignmentGroup.assignments[0].points_possible);
+console.log(AssignmentGroup.assignments[1].points_possible);
+
+console.log('Assignment IDs');
+console.log(AssignmentGroup.assignments[0].id);
+console.log(AssignmentGroup.assignments[1].id);
+
+console.log('Learner 125 scores:');
+console.log(LearnerSubmissions[0].submission.score);
+console.log(LearnerSubmissions[1].submission.score);
+
+console.log('Learner 132 scores:');
+console.log(LearnerSubmissions[3].submission.score);
+console.log((LearnerSubmissions[4].submission.score) - 15);
+
+
+// Create new variables (condense names)
+console.log('Class Averages');
+let learner125Avg = (LearnerSubmissions[0].submission.score + LearnerSubmissions[1].submission.score) / (AssignmentGroup.assignments[0].points_possible + AssignmentGroup.assignments[1].points_possible);
+console.log(learner125Avg);
+
+let learner132Avg = (LearnerSubmissions[3].submission.score + (LearnerSubmissions[4].submission.score - 15)) / (AssignmentGroup.assignments[0].points_possible + AssignmentGroup.assignments[1].points_possible);
+console.log(learner132Avg);
+
+console.log('Individual Scores');
+console.log('Learner125');
+let ass1Learner125 = (LearnerSubmissions[0].submission.score)/(AssignmentGroup.assignments[0].points_possible);
+console.log(ass1Learner125);
+let ass2Learner125 = (LearnerSubmissions[1].submission.score)/(AssignmentGroup.assignments[1].points_possible);
+console.log(ass2Learner125);
+
+console.log('Learner 132');
+let ass1Learner132 = (LearnerSubmissions[3].submission.score)/(AssignmentGroup.assignments[0].points_possible);
+console.log(ass1Learner132);
+let ass2Learner132 = ((LearnerSubmissions[4].submission.score) - 15)/(AssignmentGroup.assignments[1].points_possible);
+console.log(ass2Learner132);
